4c20c291aeaecc9dad6a130e330be616db3736b3
JeanFG <jeanfg86@gmail.com>
2021-09-07 20:22:30 -0300

    refactor: move EmailValidator creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from './signup';
-import { EmailValidator } from '../protocols/email-validator';
+import { EmailValidator } from '../protocols/index';
 import { MissingParamError, InvalidParamError, ServerError } from '../errors';
 
 interface SutTypes {
@@ -7,13 +7,26 @@ interface SutTypes {
     emailValidatorStub: EmailValidator;
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
         isValid(email: string): boolean {
             return true;
         }
     }
-    const emailValidatorStub = new EmailValidatorStub();
+    return new EmailValidatorStub();
+};
+
+const makeEmailValidatorWithError = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid(email: string): boolean {
+            throw new Error();
+        }
+    }
+    return new EmailValidatorStub();
+};
+
+const makeSut = (): SutTypes => {
+    const emailValidatorStub = makeEmailValidator();
     const sut = new SignUpController(emailValidatorStub);
 
     return {
@@ -113,12 +126,7 @@ describe('SignUp Controller ', () => {
     });
 
     test('Should return 500 if emailValidator throws', () => {
-        class EmailValidatorStub implements EmailValidator {
-            isValid(email: string): boolean {
-                throw new Error();
-            }
-        }
-        const emailValidatorStub = new EmailValidatorStub();
+        const emailValidatorStub = makeEmailValidatorWithError();
         const sut = new SignUpController(emailValidatorStub);
         const httpRequest = {
             body: {
