aa75f494175efe7be4b295669db5ad63c6a33a14
JeanFG <jeanfg86@gmail.com>
2021-10-12 15:10:30 -0300

    refactor: remove duplicated code

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -24,17 +24,24 @@ const makeEncrypter = (): Encrypter => {
 const makeAddAccountRepository = (): AddAccountRepository => {
     class AddAccountRepositoryStub implements AddAccountRepository {
         add(accountData: AddAccountModel): Promise<AccountModel> {
-            const fakeAccount = {
-                id: 'valid_id',
-                name: 'valid_name',
-                email: 'valid_email',
-                password: 'hashed_password',
-            };
-            return new Promise(resolve => resolve(fakeAccount));
+            return new Promise(resolve => resolve(makeFakeAccount()));
         }
     }
     return new AddAccountRepositoryStub();
 };
+
+const makeFakeAccount = (): AccountModel => ({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email',
+    password: 'hashed_password',
+});
+
+const makeFakeAccountData = (): AddAccountModel => ({
+    name: 'valid_name',
+    email: 'valid_email',
+    password: 'valid_password',
+});
 
 const makeSut = (): SutTypes => {
     const encrypterStub = makeEncrypter();
@@ -51,12 +58,7 @@ describe('DbAddAccount Usecase', () => {
     test('should call Encrypter with correct password', async () => {
         const { sut, encrypterStub } = makeSut();
         const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'valid_password',
-        };
-        await sut.add(accountData);
+        await sut.add(makeFakeAccountData());
         expect(encryptSpy).toHaveBeenLastCalledWith('valid_password');
     });
 
@@ -65,24 +67,14 @@ describe('DbAddAccount Usecase', () => {
         jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(
             new Promise((resolve, reject) => reject(new Error())),
         );
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'valid_password',
-        };
-        const promise = sut.add(accountData);
+        const promise = sut.add(makeFakeAccountData());
         await expect(promise).rejects.toThrow();
     });
 
     test('should call AddAccountRepository with correct values', async () => {
         const { sut, addAccountRepositoryStub } = makeSut();
         const addSpy = jest.spyOn(addAccountRepositoryStub, 'add');
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'hashed_password',
-        };
-        await sut.add(accountData);
+        await sut.add(makeFakeAccountData());
         expect(addSpy).toHaveBeenLastCalledWith({
             name: 'valid_name',
             email: 'valid_email',
@@ -95,28 +87,13 @@ describe('DbAddAccount Usecase', () => {
         jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
             new Promise((resolve, reject) => reject(new Error())),
         );
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'valid_password',
-        };
-        const promise = sut.add(accountData);
+        const promise = sut.add(makeFakeAccountData());
         await expect(promise).rejects.toThrow();
     });
 
     test('should return an account on success', async () => {
         const { sut } = makeSut();
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'hashed_password',
-        };
-        const account = await sut.add(accountData);
-        expect(account).toEqual({
-            id: 'valid_id',
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'hashed_password',
-        });
+        const account = await sut.add(makeFakeAccountData());
+        expect(account).toEqual(makeFakeAccount());
     });
 });
