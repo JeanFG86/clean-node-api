fea7aafaa0ed37b77d7c7fcb86dd90965fe57bc5
JeanFG <jeanfg86@gmail.com>
2021-10-15 10:22:36 -0300

    feat: ensure SignUpController returns 400 if validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -216,7 +216,7 @@ describe('SignUp Controller ', () => {
         expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
     });
 
-    test('Should return 200 if validation returns an error', async () => {
+    test('Should return 400 if validation returns an error', async () => {
         const { sut, validationStub } = makeSut();
         jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
             new MissingParamError('any_field'),
