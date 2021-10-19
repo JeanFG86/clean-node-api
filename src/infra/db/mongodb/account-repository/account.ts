7502508d36033857c440847cb93d5b905d5670b7
JeanFG <jeanfg86@gmail.com>
2021-10-19 17:50:05 -0300

    test: should return an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,31 +1,35 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+    implements AddAccountRepository, LoadAccountByEmailRepository
+{
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts');
         const result = await accountCollection.insertOne(accountData);
-        //console.log(result);
-
         const account = await accountCollection.findOne(result.insertedId);
 
-        //console.log(account.email);
-
-        const r = {
+        const accountReturn = {
             id: account._id,
             name: account.name,
             email: account.email,
             password: account.password,
         };
-        return r;
-        //console.log(r);
-        //const { _id, ...accountWithoutId } = account;
-        //const retorno = Object.assign({}, accountWithoutId, { id: _id });
+        return accountReturn;
+    }
 
-        //console.log(retorno);
-        //return new Promise(resolve => resolve(null));
-        //return retorno;
+    async loadByEmail(email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts');
+        const account = await accountCollection.findOne({ email });
+        const accountReturn = {
+            id: account._id,
+            name: account.name,
+            email: account.email,
+            password: account.password,
+        };
+        return accountReturn;
     }
 }
