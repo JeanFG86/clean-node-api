a2cb11e7f058c4e115c3887609dc2b8131897d88
JeanFG <jeanfg86@gmail.com>
2021-11-12 19:38:57 -0300

    feat: add AddSurvey composition

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -1,9 +1,9 @@
 import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account';
-import { AddAccountRepository } from '../../../../data/usecases/add-account/db-add-account-protocols';
+import { AddAccount } from '../../../../data/usecases/add-account/db-add-account-protocols';
 import { BcryptAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository';
 
-export const makeDbAddAccount = (): AddAccountRepository => {
+export const makeDbAddAccount = (): AddAccount => {
     const bcryptAdapter = new BcryptAdapter(12);
     const accountMongoRepository = new AccountMongoRepository();
     return new DbAddAccount(
