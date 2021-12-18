a2cc29cdfcd351fc8173d6bb9d4927774accc6d4
JeanFG <jeanfg86@gmail.com>
2021-12-18 14:53:10 -0300

    refactor: refactor

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
     mongoUrl:
         process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-    port: process.env.PORT || 3333,
+    port: process.env.PORT || 4444,
     jwtSecret: process.env.JWT_SECRET || 'thr6G9h',
 };
