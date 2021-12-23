4e654cffc91317e05a501ed224f6a34aa699d769
JeanFG <jeanfg86@gmail.com>
2021-12-22 22:20:21 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken';
 import { Decrypter } from '../../../data/protocols/criptography/decrypter';
 export class JwtAdapter implements Encrypter, Decrypter {
     constructor(private readonly secret: string) {}
-    async decrypt(value: string): Promise<string> {
-        await jwt.verify(value, this.secret);
-        return null;
+    async decrypt(token: string): Promise<string> {
+        const value: any = await jwt.verify(token, this.secret);
+        return value;
     }
     async encrypt(value: string): Promise<string> {
         const accessToken = await jwt.sign({ id: value }, this.secret);
